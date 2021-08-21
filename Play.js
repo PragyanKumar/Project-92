da1538a4de4265affc9910c987cb7d64e85b90b8
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-08-21 20:01:28 +0530

    Add files via upload

--- Play.js
+++ Play.js
@@ -34,7 +34,7 @@ function check(){
 get_answer=document.getElementById("input_check_box").value
 console.log(answer)
 
-if(answer==word){
+if(get_answer==actual_answer){
     if(answer_turn=="player1"){
         update_player1_score=player1_score+1
         document.getElementById("player1_score").innerHTML=player1_score
